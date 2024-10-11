54920db8568b1df21481f512145e01718a59ffdc
Pham Duc <ducpt@valofe.com>
2024-10-11 10:26:03 +0700

    - Remove OnReleasePoolObject

--- Runtime/Guinea/Core/ObjectPools/LinkedPool.cs
+++ Runtime/Guinea/Core/ObjectPools/LinkedPool.cs
@@ -5,14 +5,14 @@ namespace Guinea.Core.ObjectPools
 {
       public class LinkedPool<T> : UnityEngine.Pool.LinkedPool<T> where T : UnityEngine.Component
     {
-        public LinkedPool(Func<T> createFunc, Action<T> actionOnGet = null, bool collectionCheck = true, int maxSize = 10000) : base(createFunc, actionOnGet, OnReleasePoolObject, OnDestroyPoolObject, collectionCheck, maxSize){
+        public LinkedPool(Func<T> createFunc, Action<T> actionOnGet = null, bool collectionCheck = true, int maxSize = 10000) : base(createFunc, actionOnGet, null, OnDestroyPoolObject, collectionCheck, maxSize){
             
         }
 
-        private static void OnReleasePoolObject(T o)
-        {
-            o.gameObject.SetActive(false);
-        }
+        // private static void OnReleasePoolObject(T o)
+        // {
+        //     o.gameObject.SetActive(false);
+        // }
 
         private static void OnDestroyPoolObject(T o)
         {
--- Runtime/Guinea/Core/ObjectPools/ObjectPool.cs
+++ Runtime/Guinea/Core/ObjectPools/ObjectPool.cs
@@ -5,14 +5,14 @@ namespace Guinea.Core.ObjectPools
 {
     public class ObjectPool<T> : UnityEngine.Pool.ObjectPool<T> where T : UnityEngine.Component
     {
-        public ObjectPool(Func<T> createFunc, Action<T> actionOnGet = null, bool collectionCheck = true, int defaultCapacity = 10, int maxSize = 10000) : base(createFunc, actionOnGet, OnReleasePoolObject, OnDestroyPoolObject, collectionCheck, defaultCapacity, maxSize){
+        public ObjectPool(Func<T> createFunc, Action<T> actionOnGet = null, bool collectionCheck = true, int defaultCapacity = 10, int maxSize = 10000) : base(createFunc, actionOnGet, null, OnDestroyPoolObject, collectionCheck, defaultCapacity, maxSize){
             
         }
 
-        private static void OnReleasePoolObject(T o)
-        {
-            o.gameObject.SetActive(false);
-        }
+        // private static void OnReleasePoolObject(T o)
+        // {
+        //     o.gameObject.SetActive(false);
+        // }
 
         private static void OnDestroyPoolObject(T o)
         {
